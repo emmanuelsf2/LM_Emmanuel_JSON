a4149680b94ba37451e586f29dab11130727a6ef
Emmanuel Sanfo <emmsanamo@alu.edu.gva.es>
2025-01-31 17:41:54 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+// script.js
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("libros.json")
+        .then((response) => response.json())
+        .then((data) => {
+            const gallery = document.getElementById("gallery");
+            data.forEach((item) => {
+                const card = document.createElement("div");
+                card.className = "card";
+                card.innerHTML = `
+                    <img src="${item.cover}" alt="${item.cover}" class="books">
+                    <h2>${item.title}</h2>
+                    <div class="author">${item.author}</div>
+                    <p>${item.genre}</p>
+                    <p>${item.year}</p>
+                `;
+                gallery.appendChild(card);
+            });
+        })
+        .catch((error) => console.error("Error loading data:", error));
+});
